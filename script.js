3c098c12ea2850fb567dd7028853d8313b4988f2
Radhika Morabia <hi@rmorabia.com>
2018-01-29 23:30:13 -0800

    v03: halfway point, hopefully

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var r = 0
 var g = 0
 var b = 0
 
-var colorBlocks = [1, 2, 3, 4, 5, 6]
+var colorBlocks = [0, 1, 2, 3, 4, 5]
+var rgbArray = []
 
-colorBlocks[1] = document.getElementById('viewColor1')
-colorBlocks[2] = document.getElementById('viewColor2')
-colorBlocks[3] = document.getElementById('viewColor3')
-colorBlocks[4] = document.getElementById('viewColor4')
-colorBlocks[5] = document.getElementById('viewColor5')
-colorBlocks[6] = document.getElementById('viewColor6')
+colorBlocks[0] = document.getElementById('viewColor1')
+colorBlocks[1] = document.getElementById('viewColor2')
+colorBlocks[2] = document.getElementById('viewColor3')
+colorBlocks[3] = document.getElementById('viewColor4')
+colorBlocks[4] = document.getElementById('viewColor5')
+colorBlocks[5] = document.getElementById('viewColor6')
 
 function randomRGB () {
   r = Math.floor(Math.random() * 256)
@@ -17,10 +18,17 @@ function randomRGB () {
   b = Math.floor(Math.random() * 256)
 }
 
+function correctBlock () {
+  var num = (Math.floor(Math.random() * 5))
+  document.getElementById('colorText').textContent = rgbArray[num]
+}
+
 document.getElementById('colorButton').addEventListener('click', function () {
-  for (var i = 1; i < colorBlocks.length; i++) {
+  for (var i = 0; i < colorBlocks.length; i++) {
     randomRGB()
     var rgb = 'rgb(' + r + ',' + g + ',' + b + ')'
     colorBlocks[i].style.color = rgb
-}
+    rgbArray.push(rgb)
+  }
+  correctBlock();
 })
